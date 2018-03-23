b65a55d5987963a4f418b86321ba98abc805a65f
jgauthi <jgauthi@users.noreply.github.com>
2018-03-23 11:09:38 +0100

    Symfony - Install web console with coresphere/console-bundle

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 mindsymfony
 ===========
-
 A Symfony project created on January 26, 2018, 10:57 am.
+
+## Installation
+Commande lines:
+
+```bash
+git clone git@bitbucket.org:gauthij/mindsymfony.git
+git-install-hook-pre-commit.sh
+git-install-config-github-email.sh
+composer install
+php bin/console assets:install --symlink
+php bin/console doctrine:fixtures:load
+droit-linux-www.sh
+````
+
+For the asset symlink install, launch a terminal on administrator in windows environment.
+
+
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
