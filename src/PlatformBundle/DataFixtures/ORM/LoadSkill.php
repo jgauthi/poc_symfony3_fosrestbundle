1b047c08c1a9be69ae394787b2b73c5892609da2
jgauthi <jgauthi@users.noreply.github.com>
2018-03-25 12:51:07 +0200

    PlatformBundle Data Fixture - Add custom fixture function

--- src/PlatformBundle/DataFixtures/ORM/LoadSkill.php
+++ src/PlatformBundle/DataFixtures/ORM/LoadSkill.php
@@ -10,7 +10,12 @@ class LoadSkill implements FixtureInterface
 {
 	public function load(ObjectManager $manager)
 	{
-        $objects = Fixtures::load(__DIR__ . '/LoadSkill.yml', $manager);
+        $objects = Fixtures::load
+        (
+            __DIR__ . '/LoadSkill.yml',
+            $manager,
+            array('providers' => array($this))
+        );
 
 	    /*
 		// Liste des noms de compétences à ajouter
@@ -30,6 +35,31 @@ class LoadSkill implements FixtureInterface
 		$manager->flush();
 	    */
 	}
+
+	public function custom_skill()
+    {
+        static $names = array
+        (
+            'PHP', 'Symfony',
+            'Wordpress', 'Typo3',
+            'C++', 'Java',
+            'Javscript', 'jQuery',
+            'Photoshop', 'Fireworks',
+            'Blender', 'Shell script',
+            'Bloc-note', 'PhpStorm',
+            'Ruby', 'Rails',
+            'Présentation',
+            'Fixture', 'Generation',
+        );
+
+        $key = array_rand($names);
+
+        // Return a uniq random value
+        $value = $names[$key];
+        unset($names[$key]);
+
+        return $value;
+    }
 }
 
 ?>
