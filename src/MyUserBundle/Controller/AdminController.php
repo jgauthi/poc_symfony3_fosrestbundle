798a6dae41d539e1855f59eea0a523e99eaeaf28
jgauthi <jgauthi@users.noreply.github.com>
2018-11-02 16:39:07 +0100

    EasyAdminBundle - Split advert archived or not, don't display applications from archived advert

--- src/MyUserBundle/Controller/AdminController.php
+++ src/MyUserBundle/Controller/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 namespace MyUserBundle\Controller;
 
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use FOS\UserBundle\Model\UserInterface;
 
@@ -24,5 +25,20 @@ class AdminController extends BaseAdminController
     {
         $this->get('fos_user.user_manager')->updateUser($user, false);
         parent::persistEntity($user);
+    }
+
+    // Change default List request
+    public function createListQueryBuilder($entityClass, $sortDirection, $sortField = null, $dqlFilter = null): QueryBuilder
+    {
+        $response =  parent::createListQueryBuilder($entityClass, $sortDirection, $sortField, $dqlFilter);
+
+        // Display applications/advertSkill from not archived advert
+        if(preg_match('#PlatformBundle\\\Entity\\\(Application|AdvertSkill)#i', $entityClass)) {
+            $response
+                ->innerJoin('entity.advert', 'advert', 'WITH', 'advert.archived = :archived')
+                ->setParameter('archived', 0);
+        }
+
+        return $response;
     }
 }
