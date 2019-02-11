b085f2b8b0051e9f5473875c5cdc7b72ea93a7a0
jgauthi <jgauthi@users.noreply.github.com>
2019-02-11 18:56:38 +0100

    Migration to SF 3.4 LTS: Migrate tests (phpunit)

---
+++ tests/Controller/AdvertControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdvertControllerTest extends WebTestCase
+{
+    public function testHello(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/fr/platform');
+
+        $this->assertContains('Liste des annonces', $client->getResponse()->getContent());
+    }
+}
---
+++ tests/Entity/ApplicationTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Application;
+
+class ApplicationTest extends TestCase
+{
+    private function initApplication()
+    {
+        $application = new Application();
+
+        $application->setAuthor('Tyrion Lannister')
+            ->setContent('MyTestContent')
+            ->setDate(\DateTime::createFromFormat('Y-m-d', '2018-07-15'))
+            ->setCity('Port-Réal')
+            ->setSalaryClaim(5000);
+
+        return $application;
+    }
+
+    public function testApplicationHasAdvertAttribute()
+    {
+        $this->assertClassHasAttribute('advert', application::class);
+    }
+
+    public function testEqualDatetimeVar()
+    {
+        $application = $this->initApplication();
+        $date = new \DateTime();
+        $date->setDate(2018, 7, 15);
+
+        $this->assertSame($date->format('Y-m-d'), $application->getDate()->format('Y-m-d'));
+    }
+
+    /**
+     * @dataProvider dataSalaryExpected
+     */
+    public function testSeveralSalary($salary, $experted)
+    {
+        $application = new Application();
+        $application->setSalaryClaim($salary);
+
+        $this->assertLessThan($experted, $application->getSalaryClaim());
+    }
+
+    public function dataSalaryExpected()
+    {
+        // salary claim, maximum expected
+        return [
+            [2200, 2500],
+            [1500, 1800],
+            [3200, 3300],
+        ];
+    }
+}
