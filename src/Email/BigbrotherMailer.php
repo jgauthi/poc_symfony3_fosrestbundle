31c13fdf4ad3b8c323318285e8c64e59d6a68ffb
jgauthi <jgauthi@users.noreply.github.com>
2019-03-09 17:41:26 +0100

    Mailer: Bigbrother (move from App/Service)

---
+++ src/Email/BigbrotherMailer.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Email;
+
+use Swift_Message;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BigbrotherMailer extends AbstractMailer
+{
+    /**
+     * Method to notify an administrator by e-mail
+     * @param string $message
+     * @param UserInterface $user
+     * @return int|null
+     */
+    public function send(string $message, UserInterface $user): ?int
+    {
+        $message = Swift_Message::newInstance()
+            ->setSubject('New message from BigDaddy')
+            ->setFrom('bigbrother@'.self::DOMAIN_MAIL)
+            ->setTo('admin@'.self::DOMAIN_MAIL)
+            ->setBody(sprintf('"The monitored user %s posted the following message: "%s"', $user->getUsername(), $message))
+        ;
+
+        return $this->mailer->send($message);
+    }
+}
---
+++ src/EventListener/BigbrotherListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Email\BigbrotherMailer;
+use App\Event\MessagePostEvent;
+
+class BigbrotherListener
+{
+    protected $mailer;
+    protected $listUsers = [];
+
+    /**
+     * BigbrotherListener constructor.
+     *
+     * @param BigbrotherMailer $mailer
+     * @param $listUsers
+     */
+    public function __construct(BigbrotherMailer $mailer, array $listUsers)
+    {
+        $this->mailer = $mailer;
+        $this->listUsers = $listUsers;
+    }
+
+    /**
+     * @param MessagePostEvent $event
+     */
+    public function processMessage(MessagePostEvent $event): void
+    {
+        // Monitoring is activated if the author of the message is in the list
+        if (\in_array($event->getUser()->getUsername(), $this->listUsers, true)) {
+            $this->mailer->send($event->getMessage(), $event->getUser());
+        }
+    }
+}
