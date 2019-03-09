dc2c3fc9382debfbce724c575013245055e6ed30
jgauthi <jgauthi@users.noreply.github.com>
2019-03-09 19:54:14 +0100

    Command: Send Advert by Email - fix #3

---
+++ src/Command/AdvertRapportCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Command;
+
+use App\Email\AdvertRapportMailer;
+use App\Entity\Advert;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputArgument, InputInterface};
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AdvertRapportCommand extends Command
+{
+    protected static $defaultName = 'advert:rapport';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var AdvertRapportMailer
+     */
+    private $mailer;
+
+    /**
+     * AdvertRapportCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param AdvertRapportMailer $mailer
+     * @param string|null $name
+     */
+    public function __construct(EntityManagerInterface $entityManager, AdvertRapportMailer $mailer, ?string $name = null)
+    {
+        parent::__construct($name);
+        $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Advert Rapport item.')
+            ->setHelp('This command allows you to send rapport advert by mail.')
+        ;
+
+        $this->addArgument('id', InputArgument::REQUIRED, 'Get Advert by ID');
+        $this->addArgument('email', InputArgument::OPTIONAL, 'To email');
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @throws \Exception
+     *
+     * @return int|void|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $advertId = $input->getArgument('id');
+        $to = $input->getArgument('email');
+
+        $advertRepository = $this->entityManager->getRepository(Advert::class);
+        $advert = $advertRepository->find($advertId);
+
+        if (empty($advert)) {
+            die($output->writeln('<error>Advert not found.</error>'));
+        }
+
+        if(empty($to)) {
+            $to = preg_replace('#[^a-z0-9_]+#i', '-', $advert->getAuthor());
+            $to .= '@symfony.local';
+        }
+
+//        if ($this->mailer->send($to, $advert)) {
+//            $output->writeln("<fg=green>Rapport successfully to $to</>");
+//        }
+//        else $output->writeln("<error>Fail sending rapport to $to</error>");
+
+        $this->mailer->send($to, $advert); //todo: Détecter si le mail est correctement envoyé
+        $output->writeln("<fg=green>Rapport successfully to $to</>");
+    }
+}
---
+++ src/Email/AdvertRapportMailer.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Email;
+
+use App\Entity\Advert;
+use App\Entity\Application;
+use Swift_Message;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+
+class AdvertRapportMailer extends AbstractMailer
+{
+    /**
+     * @param string $to
+     * @param Advert $advert
+     * @return int|null
+     */
+    public function send(string $to, Advert $advert): ?int
+    {
+        if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('Email incorrect');
+        }
+
+        // Declare Advert Data
+        $attach = [];
+        if (!empty($advert->getCategories()->count())) {
+            $categories = [];
+            foreach ($advert->getCategories() as $category) {
+                $categories[] = $category->getName();
+            }
+            $attach[] = 'Categories: '.implode(', ', $categories);
+        }
+
+        $content = [
+            $advert->getTitle(),
+            'By: '.$advert->getAuthor().
+            ' on '.$advert->getDate()->format('Y-m-d H:i'),
+            'Attach: '.implode(', ', $attach),
+            '',
+            ''.$advert->getContent(),
+        ];
+
+        $applications = $advert->getApplications();
+        if (!empty($applications)) {
+            $content[] = '';
+            $content[] = 'Applications: ';
+
+            /** @var Application $app */
+            foreach ($applications->toArray() as $app) {
+                $content[] = "- {$app->getAuthor()} ({$app->getCity()}): {$app->getContent()}";
+            }
+        }
+
+        $message = new Swift_Message(
+            sprintf('Advert Rapport "%s"', $advert->getTitle()),
+            implode("\n", $content)
+        );
+
+        $message
+            ->addTo($to)
+            ->addFrom('admin@'.self::DOMAIN_MAIL)
+        ;
+
+        return $this->mailer->send($message);
+    }
+}
